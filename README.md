fd8dde9789997a924f6937064f01cf0fb23fd5c4
dfkai <541841235@qq.com>
2022-02-02 20:48:02 +0800

    更新文档

--- README.md
+++ README.md
@@ -21,6 +21,9 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 ### 未实现功能
 如果退出播放页面则无法播放，这个是设计的问题。
 
+### 打包
+将`yarn build`后的dist文件夹的内容放在 `tsapp/app/src/main/assets`中就可以直接用`android studio`打包了
+
 ### 预览图
  ![image](https://github.com/libaibuaidufu/vue-tsapp/blob/main/preview/搜索.jpg) 
  ![image](https://github.com/libaibuaidufu/vue-tsapp/blob/main/preview/收藏.jpg) 
--- vue.config.js
+++ vue.config.js
@@ -24,12 +24,8 @@ module.exports = {
         proxy: {  //配置跨域
             '/api': {
                 target: 'https://app.tingxiaoshuo.cc/listen/api',  //这里后台的地址模拟的;应该填写你们真实的后台接口
-                // target: 'https://ts.kaizhu.xyz/api',  //这里后台的地址模拟的;应该填写你们真实的后台接口
                 changOrigin: true,  //允许跨域
                 pathRewrite: {
-                    /* 重写路径，当我们在浏览器中看到请求的地址为：http://localhost:8080/api/core/getData/userInfo 时
-                      实际上访问的地址是：http://121.121.67.254:8185/core/getData/userInfo,因为重写了 /api
-                     */
                     '^/api': ''
                 }
             },
@@ -37,9 +33,6 @@ module.exports = {
                 target: 'https://app.tingxiaoshuo.cc/listen/apptingchina',  //这里后台的地址模拟的;应该填写你们真实的后台接口
                 changOrigin: true,  //允许跨域
                 pathRewrite: {
-                    /* 重写路径，当我们在浏览器中看到请求的地址为：http://localhost:8080/api/core/getData/userInfo 时
-                      实际上访问的地址是：http://121.121.67.254:8185/core/getData/userInfo,因为重写了 /api
-                     */
                     '^/apptingchina': ''
                 }
             },
@@ -47,9 +40,6 @@ module.exports = {
                 target: 'https://www.tingxiaoshuo.cc/pc',  //这里后台的地址模拟的;应该填写你们真实的后台接口
                 changOrigin: true,  //允许跨域
                 pathRewrite: {
-                    /* 重写路径，当我们在浏览器中看到请求的地址为：http://localhost:8080/api/core/getData/userInfo 时
-                      实际上访问的地址是：http://121.121.67.254:8185/core/getData/userInfo,因为重写了 /api
-                     */
                     '^/pc': ''
                 },
                 headers: {
