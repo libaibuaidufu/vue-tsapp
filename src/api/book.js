c63fcff4f3c7dec4ff758a839cbd0a630aad6352
libaibuaidufu <libaibuaidufu@gmail.com>
2023-05-02 19:58:46 +0800

    只是换了域名，接口还是一样，修改错了，草
    只是换了域名，接口还是一样，修改错了，草

--- src/api/book.js
+++ src/api/book.js
@@ -1,17 +1,17 @@
 import axios from './requests'
 
 // 开发环境
-// export const bookInfo=(params)=>axios.get('/api/book',params)
-// export const bookListen=(params)=>axios.get('/api/chapter',params)
-// export const bookSearch=(params)=>axios.get("/apptingchina/appSearch",params)
-// export const bookOne=(params)=>axios.get('/apptingchina/AppGetChapterUrl',params)
-// export const bookChapter=(params)=>axios.get('https://127.0.0.1/api/bookChapter',params)
+export const bookInfo=(params)=>axios.get('/api/book',params)
+export const bookListen=(params)=>axios.get('/api/chapter',params)
+export const bookSearch=(params)=>axios.get("/apptingchina/appSearch",params)
+export const bookOne=(params)=>axios.get('/apptingchina/AppGetChapterUrl',params)
+export const bookChapter=(params)=>axios.get('/apptingchina/AppGetChapterUrl2022',params)
 
 
 // app打包使用
-export const bookInfo=(params)=>axios.get('https://app.tingxiaoshuo.cc/listen/api/book',params)
-export const bookListen=(params)=>axios.get('https://app.tingxiaoshuo.cc/listen/api/chapter',params)
-export const bookSearch=(params)=>axios.get("https://app.tingxiaoshuo.cc/listen/apptingchina/appSearch",params)
-export const bookOne=(params)=>axios.get('https://app.tingxiaoshuo.cc/listen/apptingchina/AppGetChapterUrl',params)
-export const bookChapter=(params)=>axios.get('https://127.0.0.1/api/bookChapter',params)
+// export const bookInfo=(params)=>axios.get('https://app.tingxiaoshuo.cc/listen/api/book',params)
+// export const bookListen=(params)=>axios.get('https://app.tingxiaoshuo.cc/listen/api/chapter',params)
+// export const bookSearch=(params)=>axios.get("https://app.tingxiaoshuo.cc/listen/apptingchina/appSearch",params)
+// export const bookOne=(params)=>axios.get('https://app.tingxiaoshuo.cc/listen/apptingchina/AppGetChapterUrl',params)
+// export const bookChapter=(params)=>axios.get('https://127.0.0.1/api/bookChapter',params)
 
--- vue.config.js
+++ vue.config.js
@@ -24,14 +24,16 @@ module.exports = {
     devServer: {
         proxy: {  //配置跨域
             '/api': {
-                target: 'https://app.tingxiaoshuo.cc/listen/api',  //这里后台的地址模拟的;应该填写你们真实的后台接口
+                // target: 'https://app.tingxiaoshuo.cc/listen/api',  //这里后台的地址模拟的;应该填写你们真实的后台接口
+                target: 'https://app.tings8.com/listen/api',  //这里后台的地址模拟的;应该填写你们真实的后台接口
                 changOrigin: true,  //允许跨域
                 pathRewrite: {
                     '^/api': ''
                 }
             },
             '/apptingchina':{
-                target: 'https://app.tingxiaoshuo.cc/listen/apptingchina',  //这里后台的地址模拟的;应该填写你们真实的后台接口
+                // target: 'https://app.tingxiaoshuo.cc/listen/apptingchina',  //这里后台的地址模拟的;应该填写你们真实的后台接口
+                target: 'https://app.tings8.com/listen/apptingchina',  //这里后台的地址模拟的;应该填写你们真实的后台接口
                 changOrigin: true,  //允许跨域
                 pathRewrite: {
                     '^/apptingchina': ''
